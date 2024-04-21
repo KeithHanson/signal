17d8fb3fc6594fa694fb8ee08d66789fa4a9d8b5
Keith Hanson <keith@keithhanson.io>
2024-04-21 13:21:15 -0500

    Multi hit for map working.

--- tests/test_space_room.py
+++ tests/test_space_room.py
@@ -10,6 +10,8 @@ class TestSpaceRoom(EvenniaCommandTest):
         super().setUp()
 
         self.ship = create.create_object( DefaultSpaceShip, key="ship" )
+        self.ship2 = create.create_object( DefaultSpaceShip, key="ship2" )
+        self.ship3 = create.create_object( DefaultSpaceShip, key="ship3" )
 
         self.space_room = create.create_object( SpaceRoom, key="space_room" )
 
@@ -36,6 +38,22 @@ class TestSpaceRoom(EvenniaCommandTest):
 20 | . . . . . @ . . . . . 
 21 | . . . . . . . . . . . 
 22 | . . . . . . . . . . . 
+23 | . . . . . . . . . . . 
+24 | . . . . . . . . . . . 
+25 | . . . . . . . . . . . 
+    ----------------------
+             x:20
+"""
+
+        self.test_map_multihit = """
+15 | . . . . . . . . . . . 
+16 | . @ . . . . . . . . . 
+17 | . . . . . . . . . . . 
+18 | . . . . . . . . . . . 
+19 | . . . . . . . . . . . 
+20 | . . . . . @ . . . . . 
+21 | . . . . . . . . . . . 
+22 | . . . . . @ . . . . . 
 23 | . . . . . . . . . . . 
 24 | . . . . . . . . . . . 
 25 | . . . . . . . . . . . 
@@ -80,4 +98,22 @@ class TestSpaceRoom(EvenniaCommandTest):
 
             self.assertEqual(test_string, rendered_string)
 
-        print(self.rendered_map)
+    def test_multiple_items_rendered(self):
+        self.call(CmdPilotLaunch(), "")
+
+        self.ship2.move_to(self.space_room)
+        self.ship2.newtonian_data["x"] = 20
+        self.ship2.newtonian_data["y"] = 22
+
+        self.ship3.move_to(self.space_room)
+        self.ship3.newtonian_data["x"] = 16 
+        self.ship3.newtonian_data["y"] = 16
+
+        caller = self.char1
+        self.rendered_map = self.space_room.render_map(caller)
+
+        for row in range(0, len(self.test_map.split("\n"))):
+            test_string = self.test_map_multihit.split("\n")[row]
+            rendered_string = self.rendered_map.split("\n")[row]
+
+            self.assertEqual(test_string, rendered_string)
