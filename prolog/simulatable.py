e934a44889e7cabf28540b12a2036633f3eb9af6
Keith Hanson <keith@keithhanson.io>
2024-04-21 18:45:35 -0500

    Simulatable basic testing done. Allows you to inherit this mixin, and then track things to simulate, and simulate them, using prolog to do it.

--- prolog/simulatable.py
+++ prolog/simulatable.py
@@ -6,23 +6,45 @@ import clingo
 class Simulatable:
     to_simulate = []
 
-    def program():
+    @classmethod
+    def program(cls):
         pass
 
-    def simulate():
+    @classmethod
+    def simulate(cls):
+        # create controller
+        ctl = clingo.Control()
+
+        # Add program
+        ctl.add("base", [], cls.program())
+
+        # Add all to_simulate[] terms
+        for item in cls.to_simulate:
+            fact = item.to_fact()
+            ctl.add("base", [], fact)
+
+        # Ground
+        ctl.ground([("base", [])])
+
+        # Solve
+        ctl.solve(on_model=cls.update)
+        ctl.cleanup()
         pass
 
-    def update(model):
+    @classmethod
+    def update(cls, model):
+        # Catch solved model, interpret terms into updates for tracked objects
         pass
 
-    def track(instance):
-        to_simulate.append(instance)
+    @classmethod
+    def track(cls, instance):
+        cls.to_simulate.append(instance)
 
-    def ignore(instance):
-        to_simulate.remove(instance)
+    @classmethod
+    def ignore(cls, instance):
+        cls.to_simulate.remove(instance)
 
     def to_fact(self):
-        class_name = type(self).__name__
-        attributes = obj.attributes.all()
+        raise Exception("Must be overriden in the base class")
 
-        for attribute in attributes:
+
---
+++ tests/test_simulatable.py
@@ -0,0 +1,47 @@
+from prolog.simulatable import Simulatable
+
+from evennia.utils import create
+from evennia.utils.test_resources import EvenniaTest, EvenniaCommandTest
+from unittest.mock import patch, MagicMock
+from typeclasses.objects import Object
+
+class TestSimulatable(EvenniaCommandTest):
+    def setUp(self):
+        """Called before every test method"""
+        super().setUp()
+
+    def test_smoke_test(self):
+        class MySimulatable(Object, Simulatable):
+            key="my_sim"
+            color="red"
+            opposite=None
+
+            @classmethod
+            def program(cls):
+                return """
+                color(blue) :- opposite(red).
+                opposite(red) :- color(blue).
+
+                color(red) :- opposite(blue).
+                opposite(blue) :- color(red).
+
+                color(green) :- opposite(green).
+                opposite(green) :- color(green).
+
+                #show opposite/1.
+                """
+
+            def to_fact(self):
+                return f"color({self.color})." 
+
+            @classmethod
+            def update(cls, model):
+                symbol = str(model.symbols(shown=True)[0])
+                item = cls.to_simulate[0]
+
+                item.opposite = symbol.split("(")[1].replace(")", "")
+
+        sim = MySimulatable()
+        MySimulatable.track(sim)
+        MySimulatable.simulate()
+        self.assertEqual(sim.opposite, "blue")
